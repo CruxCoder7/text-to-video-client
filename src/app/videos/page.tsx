b4d4931b4c8a89bcf9c192109748508bd8d89041
R Akash <akashrangesh07@gmail.com>
2024-02-04 16:14:02 +0530

    Refactor video page component

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,46 +1,47 @@
-import { Video } from "@/components/video"
+import { Video } from '@/components/video';
 
 type Video_video_url = {
-  english: string
-  tamil: string | null
-  hindi: string | null
-  telugu: string | null
-  malayalam: string | null
-}
+  english: string;
+  tamil: string | null;
+  hindi: string | null;
+  telugu: string | null;
+  malayalam: string | null;
+};
 
-type Video_audio_url = Video_video_url
+type Video_audio_url = Video_video_url;
 
 type VideoType = {
-  id: string
-  content: string
-  title: string
-  video_url: Video_video_url
-  audio_url: Video_audio_url
-  images: string[]
-  enhanced_images: string[]
-}
+  id: string;
+  content: string;
+  title: string;
+  video_url: Video_video_url;
+  audio_url: Video_audio_url;
+  images: string[];
+  enhanced_images: string[];
+};
 
 export default async function Component() {
   const response = await fetch(
-    "http://test-akash.eastus.cloudapp.azure.com/api/videos",
+    'http://test-akash.eastus.cloudapp.azure.com/api/videos',
     {
-      cache: "no-store",
+      cache: 'no-store',
     }
-  )
+  );
 
-  const video: VideoType[] = await response.json()
-  console.log(video.length)
+  const video: VideoType[] = await response.json();
+  console.log(video.length);
 
   return (
     <div className="flex flex-col items-center min-h-screen p-4">
-      {video.map((video) => (
+      {video.map((video, i) => (
         <Video
           summary={video.content}
           title={video.title}
           audio_url={video.audio_url}
           video_url={video.video_url}
+          key={i}
         />
       ))}
     </div>
-  )
+  );
 }
